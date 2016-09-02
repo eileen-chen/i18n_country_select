67c9729f4cb2b1686d8265e1c4683eefda700ab6
Wei-Lin Chen <weilin.chen@agleansoft.com>
2016-09-02 16:55:48 +0800

    test function

--- lib/i18n_country_select/instance_tag.rb
+++ lib/i18n_country_select/instance_tag.rb
@@ -21,7 +21,7 @@ module I18nCountrySelect
 
       if priority_countries
         countries += options_for_select(priority_countries, selected)
-        countries += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
+        #countries += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
       end
 
       countries = countries + options_for_select(country_translations, selected)
